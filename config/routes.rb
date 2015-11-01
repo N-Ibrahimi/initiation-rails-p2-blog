d1db90987ec582746c3b848bec7b8296b67456dc
Julien Grillot <julien.grillot@gmail.com>
2015-11-01 12:21:44 +0100

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+
+  get 'articles/:id' => 'articles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
